db827693f3a6af842ef4bb967237cb69953e5b41
Rodrigo Bergamin <rodrigobergamindev@gmail.com>
2021-09-21 16:53:41 -0300

    .

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,15 @@
 import { AppProps } from 'next/app';
 import '../styles/globals.scss';
+import React from 'react';
+
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />;
+    </>
+  )
+ 
 }
 
 export default MyApp;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,15 @@
 import { GetStaticProps } from 'next';
 
 import { getPrismicClient } from '../services/prismic';
-
+import Prismic from '@prismicio/client'
 import commonStyles from '../styles/common.module.scss';
 import styles from './home.module.scss';
+import {RichText} from 'prismic-dom'
+import {useState} from 'react'
+import Link from 'next/link'
+
+import { format } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
 
 interface Post {
   uid?: string;
@@ -24,13 +30,82 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+ export default function Home({ postsPagination }: HomeProps) {
 
-// export const getStaticProps = async () => {
-//   // const prismic = getPrismicClient();
-//   // const postsResponse = await prismic.query(TODO);
+  const [nextPageLink, setNextPageLink] = useState(postsPagination.next_page);
 
-//   // TODO
-// };
+  const [goNextPage, setGoNextPage] = useState(false)
+  
+  const [postData, setPostData] = useState(postsPagination.results)
+
+  const handleNextPage = () => {
+    setGoNextPage(!goNextPage)
+
+  fetch(nextPageLink)
+    .then(response => response.json())
+    .then(function(data) {
+      console.log(data)
+      const dataToLoad = data.results.map(post => {
+        return {        
+          uid: post.uid,
+          first_publication_date: post.first_publication_date,
+          data: {
+            title: post.data.title,
+            subtitle: post.data.subtitle,
+            author: post.data.author,
+          },
+        }
+      }
+    )
+
+      setPostData(postData.concat(dataToLoad))
+      setNextPageLink(data?.next_page)
+    }) 
+
+
+  }
+
+  return (
+    <>
+    <h1>Posts</h1>
+
+    <div>
+    {postData?.map(posts => (
+            console.log(posts)
+            ))}
+    </div>
+
+    </>
+  )
+   
+}
+
+ export const getStaticProps = async () => {
+    const prismic = getPrismicClient();
+  
+    const postsResponse = await prismic.query([
+      Prismic.Predicates.at('document.type', 'posts')
+    ], {
+      fetch: ['posts.title', 'posts.subtitle', 'posts.author','posts.content'],
+      pageSize: 1,
+    });
+  
+  
+    const posts = postsResponse.results.map(post => {
+      return {
+        uid: post.uid,
+        first_publication_date: post.first_publication_date,
+        data: {
+          title: post.data.title,
+          subtitle: post.data.subtitle,
+          author: post.data.author,
+        },
+        
+      }
+    })  
+  
+    return {
+      props: {postsPagination: {results: posts, next_page: postsResponse.next_page}}
+    }
+    
+ }
